10638d3229ab9cf0b82cf2f8696522eb65134dab
roshan <roshan.ramesh1226@gmail.com>
2026-01-18 16:19:25 +0530

    if else

---
+++ if else.py
@@ -0,0 +1,8 @@
+# Python code using if-else based on user input
+
+number = int(input("Enter a number: "))
+
+if number > 0:
+    print("Prince and Lithiksha love together.")
+else:
+    print("Prince and Lithiksha
